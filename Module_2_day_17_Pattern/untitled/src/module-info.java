5063b824c3523bba60a4cdb3911b7256e6628a99
Phamtung1802 <phamtung.0218@gmail.com>
2020-08-03 08:48:36 +0700

    up

---
+++ Module_2_day_17_Pattern/untitled/src/module-info.java
@@ -0,0 +1,9 @@
+module untitled {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    opens sample;
+}    requires javafx.fxml;
+        requires javafx.controls;
+        requires javafx.graphics;
+        opens sample;
